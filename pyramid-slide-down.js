3728a51574fde0b5db285b7e50289b680d9fe921
George Tzinetas <krimini.kriminals@gmail.com>
2017-09-17 08:39:48 +0300

    Added problem description

--- pyramid-slide-down.js
+++ pyramid-slide-down.js
@@ -1,3 +1,17 @@
+/*###Lyrics... Pyramids are amazing! Both in architectural and mathematical sense. 
+* If you have a computer, you can mess with pyramids even if you are not in Egypt at the time. 
+* For example, let's consider the following problem. Imagine that you have a plane pyramid 
+* built of numbers, like this one here:
+* 
+*    /3/
+*   \7\ 4 
+*  2 \4\ 6 
+* 8 5 \9\ 3
+* Let's say that the 'slide down' is a sum of consecutive numbers from the top to the bottom of the pyramid. As you can see, the longest 'slide down' is 3 + 7 + 4 + 9 = 23
+* Your task is to write a function longestSlideDown  that takes a pyramid representation as argument 
+* and returns its' longest 'slide down'. For example,
+*/
+
 function longestSlideDown (pyramid) {
   let height = pyramid.length;
   for (let k = height - 2; k >=0; k--) {
